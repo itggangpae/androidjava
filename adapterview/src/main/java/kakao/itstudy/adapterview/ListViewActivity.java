87c8c426292a4e71eec7b4973ff9d2ccb63b3d2e
adam <itggangpae>
2021-04-06 11:44:34 +0900

    0406

--- adapterview/src/main/java/kakao/itstudy/adapterview/ListViewActivity.java
+++ adapterview/src/main/java/kakao/itstudy/adapterview/ListViewActivity.java
@@ -2,12 +2,16 @@ package kakao.itstudy.adapterview;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ListView;
 
 import com.google.android.material.snackbar.Snackbar;
@@ -37,9 +41,18 @@ public class ListViewActivity extends AppCompatActivity {
 
         //android.R 로 시작하면 안드로이드에서 제공하는 리소스
         //R 로 시작하면 사용자가 삽입한 리소스
+        /*
         adapter = new ArrayAdapter<>(
                 ListViewActivity.this,
                 android.R.layout.simple_list_item_1,
+                list);
+
+         */
+
+        //라디오 버튼을 옆에 배치하는 모양으로 설정
+        adapter = new ArrayAdapter<>(
+                ListViewActivity.this,
+                android.R.layout.simple_list_item_single_choice,
                 list);
 
         //ListView에 Adapter 설정
@@ -48,6 +61,8 @@ public class ListViewActivity extends AppCompatActivity {
         //구분선 설정
         listview.setDivider(new ColorDrawable(Color.CYAN));
         listview.setDividerHeight(3);
+        //listview의 선택모드 설정
+        listview.setChoiceMode(ListView.CHOICE_MODE_SINGLE);
 
         //ListView에서 항목을 클릭했을 때 처리
         listview.setOnItemClickListener(
@@ -65,6 +80,71 @@ public class ListViewActivity extends AppCompatActivity {
             }
         });
 
+        //데이터 삽입 버튼을 눌렀을 때 처리
+        Button btninsert = (Button)findViewById(R.id.btninsert);
+        btninsert.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //입력한 내용 가져오기
+                EditText newitem = (EditText)findViewById(R.id.newitem);
+                String item = newitem.getText().toString().trim();
+                //전송하기 전에 유효성 검사를 수행 - 널 체크, 중복 검사 등 등
+                if(item.length() <= 0){
+                    Snackbar.make(getWindow().getDecorView().getRootView(),
+                            "아이템은 필수 입력입니다.", Snackbar.LENGTH_LONG)
+                            .show();
+                    return;
+                }
+                //list의 데이터를 순회하면서 모두 대문자로 변경해서 비교해서 중복 검사
+                for(String spring:list){
+                    if(spring.toUpperCase().equals(item.toUpperCase())) {
+                        Snackbar.make(getWindow().getDecorView().getRootView(),
+                                "아이템은 중복 될 수 없습니다.", Snackbar.LENGTH_LONG)
+                                .show();
+                        return;
+                    }
+                }
+                //list에 삽입
+                list.add(item);
+                //listview 업데이트
+                adapter.notifyDataSetChanged();
+                newitem.setText("");
+                Snackbar.make(getWindow().getDecorView().getRootView(),
+                        "아이템이 정상적으로 추가되었습니다.", Snackbar.LENGTH_LONG)
+                        .show();
+                InputMethodManager imm = (InputMethodManager)getSystemService(Context.INPUT_METHOD_SERVICE);
+                imm.hideSoftInputFromWindow(newitem.getWindowToken(), 0);
 
+            }
+        });
+
+        //삭제 버튼의 클릭 이벤트 처리
+        Button btndelete = (Button)findViewById(R.id.btndelete);
+        btndelete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //작업을 수행할 데이터 유효성 검사
+                //선택된 항목의 인덱스를 찾아오기
+                int pos = listview.getCheckedItemPosition();
+                //인덱스는 데이터 범위 내의 인덱스이어야 합니다.
+                if(pos < 0 || pos >= list.size()){
+                    Snackbar.make(getWindow().getDecorView().getRootView(),
+                            "아이템이 선택되지 않아서 삭제할 수 없습니다.", Snackbar.LENGTH_LONG)
+                            .show();
+                    return;
+                }
+
+                //작업을 수행
+                //데이터 삭제하고 다시 출력하고 선택 된 항목 해제
+                list.remove(pos);
+                adapter.notifyDataSetChanged();
+                listview.clearChoices();
+
+                //수행 결과를 출력
+                Snackbar.make(getWindow().getDecorView().getRootView(),
+                        "아이템이 정상적으로 삭제되었습니다.", Snackbar.LENGTH_LONG)
+                        .show();
+            }
+        });
     }
 }
