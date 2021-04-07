42e3b4a8fafc124feeffc38daf5388bd341ce1db
adam <itggangpae>
2021-04-07 16:20:17 +0900

    0407

--- adapterview/src/main/java/kakao/itstudy/adapterview/MovieLinkActivity.java
+++ adapterview/src/main/java/kakao/itstudy/adapterview/MovieLinkActivity.java
@@ -2,7 +2,11 @@ package kakao.itstudy.adapterview;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 public class MovieLinkActivity extends AppCompatActivity {
 
@@ -10,5 +14,21 @@ public class MovieLinkActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_movie_link);
+
+        //데이터 가져오기
+        Intent intent = getIntent();
+        String link = intent.getStringExtra("link");
+
+        //WebView 찾아와서 link를 출력하기
+        WebView webview = (WebView)findViewById(R.id.webview);
+        //리다이렉트 되는 url일 때 크롬을 사용하지 않도록 설정
+        webview.setWebViewClient(new WebViewClient());
+
+        //자바스크립트를 사용할 수 있도록 설정
+        WebSettings settings = webview.getSettings();
+        settings.setJavaScriptEnabled(true);
+
+        //링크 출력
+        webview.loadUrl(link);
     }
 }
--- adapterview/src/main/java/kakao/itstudy/adapterview/MovieListActivity.java
+++ adapterview/src/main/java/kakao/itstudy/adapterview/MovieListActivity.java
@@ -4,6 +4,7 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.ColorFilter;
@@ -15,9 +16,12 @@ import android.os.Looper;
 import android.os.Message;
 import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.ProgressBar;
+
+import com.google.android.material.snackbar.Snackbar;
 
 import org.json.JSONArray;
 import org.json.JSONObject;
@@ -55,6 +59,94 @@ public class MovieListActivity extends AppCompatActivity {
         }
     };
 
+
+    //마지막 항목 뷰가 스크롤에 의해서 보여진 것인지를 판별할 프로퍼티
+    boolean lastItemVisibleFlag = false;
+
+    //페이지 번호를 저장할 프로퍼티
+    int page = 1;
+
+    //데이터를 다운로드 받는 스레드 클래스
+    class ThreadEx extends Thread{
+        public void run(){
+            try{
+                //URL을 생성 - 이 때 한글이 포함되었는지 확인하고 한글이 있으면 인코딩해서 작성
+                URL url = new URL("http://cyberadam.cafe24.com/movie/list?page=" + page);
+                //HttpURLConnection을 생성
+                HttpURLConnection con = (HttpURLConnection)url.openConnection();
+                //옵션 설정 - 전송방식 그리고 파라미터를 확인
+                con.setRequestMethod("GET");
+                con.setConnectTimeout(30000);
+                con.setUseCaches(false);
+                //POST 방식이면 파라미터에 파일이 포함되었는지 여부에 따라 코드를 추가해야 함
+
+                //문자열을 다운로드 받아서 저장할 객체 와 스트림을 생성
+                StringBuilder sb = new StringBuilder();
+                BufferedReader br = new BufferedReader(
+                        new InputStreamReader(con.getInputStream()));
+
+                //문자열을 줄 단위로 읽어서 sb에 추가
+                while(true){
+                    //한 줄을 가져오기
+                    String line = br.readLine();
+                    //읽은 내용이 없으면 더 이상 가져오지 않음
+                    if(line == null){
+                        break;
+                    }
+                    //읽은 내용이 있을 때는 line에 추가
+                    sb.append(line + "\n");
+                }
+                //연결을 해제
+                br.close();
+                con.disconnect();
+
+                //데이터 확인
+                Log.e("다운로드 받은 문자열", sb.toString());
+
+                //전체 문자열을 JSONObject로 변환
+                JSONObject root = new JSONObject(sb.toString());
+
+                //데이터 개수 가져오기
+                count = root.getInt("count");
+
+                //영화 목록 가져오기
+                JSONArray movies = root.getJSONArray("list");
+                //영화목록 순회하기
+                for(int i=0; i<movies.length(); i++){
+                    //하나의 데이터 가져오기
+                    JSONObject item = movies.getJSONObject(i);
+                    //하나의 행을 저장할 객체를 생성
+                    Movie movie = new Movie();
+
+                    //각 속성을 가져와서 movie에 대입
+                    movie.setTitle(item.getString("title"));
+                    movie.setSubtitle(item.getString("subtitle"));
+                    movie.setRating(item.getDouble("rating"));
+                    movie.setLink(item.getString("link"));
+                    movie.setThumbnail(item.getString("thumbnail"));
+
+                    //파싱한 데이터를 List에 대입
+                    movieList.add(movie);
+                }
+                Log.e("파싱한 데이터", movieList.toString());
+
+                //핸들러에게 메시지 전송
+                Message msg = new Message();
+                handler.sendMessage(msg);
+
+                //보낼 데이터가 없을 때는 아래처럼 작성해도 됨
+                //handler.sendEmptyMessage(1);
+
+            }catch(Exception e){
+                Log.e("다운로드 또는 파싱 예외", e.getLocalizedMessage());
+            }
+        }
+    }
+
+    //스레드 클래스의 객체를 참조할 프로퍼티
+    //여기에 만든 이유는 스레드가 동작 중이면 다른 스레드를 생성하지 못하도록 하기 위해서
+    ThreadEx th;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -85,81 +177,28 @@ public class MovieListActivity extends AppCompatActivity {
         //다운로드 받고 JSON 파싱을 수행한 후 재출력을 위해서 핸들러를 호출하는
         //스레드를 생성하고 실행
         new Thread(){
-            public void run(){
-                try{
-                    //URL을 생성 - 이 때 한글이 포함되었는지 확인하고 한글이 있으면 인코딩해서 작성
-                    URL url = new URL("http://cyberadam.cafe24.com/movie/list");
-                    //HttpURLConnection을 생성
-                    HttpURLConnection con = (HttpURLConnection)url.openConnection();
-                    //옵션 설정 - 전송방식 그리고 파라미터를 확인
-                    con.setRequestMethod("GET");
-                    con.setConnectTimeout(30000);
-                    con.setUseCaches(false);
-                    //POST 방식이면 파라미터에 파일이 포함되었는지 여부에 따라 코드를 추가해야 함
 
-                    //문자열을 다운로드 받아서 저장할 객체 와 스트림을 생성
-                    StringBuilder sb = new StringBuilder();
-                    BufferedReader br = new BufferedReader(
-                            new InputStreamReader(con.getInputStream()));
-
-                    //문자열을 줄 단위로 읽어서 sb에 추가
-                    while(true){
-                        //한 줄을 가져오기
-                        String line = br.readLine();
-                        //읽은 내용이 없으면 더 이상 가져오지 않음
-                        if(line == null){
-                            break;
-                        }
-                        //읽은 내용이 있을 때는 line에 추가
-                        sb.append(line + "\n");
-                    }
-                    //연결을 해제
-                    br.close();
-                    con.disconnect();
-
-                    //데이터 확인
-                    Log.e("다운로드 받은 문자열", sb.toString());
-
-                    //전체 문자열을 JSONObject로 변환
-                    JSONObject root = new JSONObject(sb.toString());
-
-                    //데이터 개수 가져오기
-                    count = root.getInt("count");
-
-                    //영화 목록 가져오기
-                    JSONArray movies = root.getJSONArray("list");
-                    //영화목록 순회하기
-                    for(int i=0; i<movies.length(); i++){
-                        //하나의 데이터 가져오기
-                        JSONObject item = movies.getJSONObject(i);
-                        //하나의 행을 저장할 객체를 생성
-                        Movie movie = new Movie();
-
-                        //각 속성을 가져와서 movie에 대입
-                        movie.setTitle(item.getString("title"));
-                        movie.setSubtitle(item.getString("subtitle"));
-                        movie.setRating(item.getDouble("rating"));
-                        movie.setLink(item.getString("link"));
-                        movie.setThumbnail(item.getString("thumbnail"));
-
-                        //파싱한 데이터를 List에 대입
-                        movieList.add(movie);
-                    }
-                    Log.e("파싱한 데이터", movieList.toString());
-
-                    //핸들러에게 메시지 전송
-                    Message msg = new Message();
-                    handler.sendMessage(msg);
-
-                    //보낼 데이터가 없을 때는 아래처럼 작성해도 됨
-                    //handler.sendEmptyMessage(1);
-
-                }catch(Exception e){
-                    Log.e("다운로드 또는 파싱 예외", e.getLocalizedMessage());
-                }
-            }
         }.start();
 
+        //ListView의 항목을 클릭했을 때 이벤트 처리
+        listView.setOnItemClickListener(
+                new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view,
+                                    int position, long id) {
+                Snackbar.make(view, "선택", Snackbar.LENGTH_LONG).show();
+                //하위 Activity에게 전달할 데이터 가져오기
+                String link = movieList.get(position).getLink();
+                //하위 Activity를 출력할 Intent 생성
+                Intent intent = new Intent(
+                        MovieListActivity.this,
+                        MovieLinkActivity.class);
+                //전달할 데이터를 저장
+                intent.putExtra("link", link);
+                //하위 Activity 출력
+                startActivity(intent);
+            }
+        });
 
     }
 }
